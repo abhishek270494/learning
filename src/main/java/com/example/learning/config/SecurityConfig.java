19366b7e34ec93ebf95a211117933a5c73970715
abhishek270494 <abhishekdhoorundhur@gmail.com>
2026-02-23 11:24:40 +0400

    Added Keycloak Integration

--- src/main/java/com/example/learning/config/SecurityConfig.java
+++ src/main/java/com/example/learning/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.example.learning.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -15,7 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
+
+    /*@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
             .csrf(CsrfConfigurer::disable)
@@ -39,5 +43,35 @@ public class SecurityConfig {
             .roles("USER")
             .build();
         return new InMemoryUserDetailsManager(user);
+    }*/
+
+    /**
+     * Using keycloak for authentication and authorization, so we disable the default form login and HTTP Basic authentication provided by Spring Security. Instead, we configure the application to use JWT tokens issued by Keycloak for securing the API endpoints.
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/todos/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder
+                .withJwkSetUri("http://keycloak:8080/realms/spring-realm/protocol/openid-connect/certs")
+                .build();
+        // Skip issuer validation
+        jwtDecoder.setJwtValidator(token -> OAuth2TokenValidatorResult.success());
+        return jwtDecoder;
     }
 }
