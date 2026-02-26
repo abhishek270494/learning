8765c2e6bf763f9c0a88cfe47e2c063a748ea344
abhishek270494 <abhishekdhoorundhur@gmail.com>
2026-02-26 14:58:39 +0400

    Added jenkins configurations

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    agent any
+
+    environment {
+        IMAGE_NAME = "spring-app"
+        CONTAINER_NAME = "spring-app"
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/abhishek270494/learning.git'
+            }
+        }
+
+        stage('Build JAR') {
+            steps {
+                sh './mvnw clean package -DskipTests'
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                sh 'docker build -t $IMAGE_NAME .'
+            }
+        }
+
+        stage('Stop Old Container') {
+            steps {
+                sh 'docker stop $CONTAINER_NAME || true'
+                sh 'docker rm $CONTAINER_NAME || true'
+            }
+        }
+
+        stage('Run New Container') {
+            steps {
+                sh '''
+                docker run -d \
+                  --name $CONTAINER_NAME \
+                  -p 8080:8080 \
+                  $IMAGE_NAME
+                '''
+            }
+        }
+    }
+}
